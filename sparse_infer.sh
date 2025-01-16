e2e4bc988dc87b590b87b65beb0cbdbb151ffc3a
lingdoudoulingling <2789105710@qq.com>
2025-01-16 15:01:28 +0800

    Update sparse_infer.sh
    Path error

--- sparse_infer.sh
+++ sparse_infer.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python inference.py \
 --super_resolution \
 --base_model_path "./checkpoint/MVD_weights/" \
---source_imgs_dir ".dataset/DTU_3views_save_warp-2/scan21_colmap/reference_images/" \
---warp_root_dir ".dataset/DTU_3views_save_warp-2/scan21_colmap/warp_images/" \
+--source_imgs_dir "./dataset/DTU_3views_save_warp-2/scan21_colmap/reference_images/" \
+--warp_root_dir "./dataset/DTU_3views_save_warp-2/scan21_colmap/warp_images/" \
 --output_dir "./output/scan21/" 
